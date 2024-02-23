98a6c342714cfad5d5c228a6fc96b2fa73476ad6
samdamazio <128763505+samdamazio@users.noreply.github.com>
2024-02-23 21:53:18 +0000

    surprise MB

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import './Home.css'; // Importando o arquivo CSS
+
 function Home() {
-    return <h1>Teste</h1>
+    return (
+        <div className="container">
+            <header>
+                <h1>AI for Business</h1>
+                <h2>Automation solutions with IAs</h2>
+            </header>
+            <main>
+                <p>Nosso pedacinho de internet tá aqui, dia 30 terá uma página foda construída aqui!</p>
+            </main>
+            <footer>
+                <p>Founders: Samuel Damazio & Martins Braga</p>
+            </footer>
+        </div>
+    );
 }
 
 export default Home;
