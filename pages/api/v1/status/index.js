fe63e394aa92d0bf46d78dc2e2eb9fe60ed84323
samdamazio <128763505+samdamazio@users.noreply.github.com>
2024-03-04 15:43:50 +0000

    database e .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({"chave":"valor"});
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  response.status(200).json({ chave: "valor" });
+  console.log(result.rows);
 }
 
-export default status
+export default status;
