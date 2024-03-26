e79f3b7602fa5d2b12348335dce41f4cb40b42ac
Sam Damazio <128763505+samdamazio@users.noreply.github.com>
2024-03-26 21:08:33 +0000

    configurando o jest usando os pacootes do Next e implementando novos tests para migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,33 +3,32 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
+  const defaultMigrationsOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    migrationsTable: "pgmigrations",
+    dir: join("infra", "migrations"),
+    direction: "up",
+    dryRun: true,
+    verbose: true,
+  };
+
   if (request.method === "GET") {
-    console.log("Entrou no GET")
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      migrationsTable: "pgmigrations",
-      dir: join("infra", "migrations"),
-      direction: "up",
-      dryRun: true,
-      verbose: true,
-    });
-    return response.status(200).json(migrations);
+    console.log("Entrou no GET");
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    return response.status(200).json(pendingMigrations);
   }
 
   if (request.method === "POST") {
-    console.log("Entrou no POST")
-    const migrations = await migrationRunner({
-
-      databaseUrl: process.env.DATABASE_URL,
-      migrationsTable: "pgmigrations",
-      dir: join("infra", "migrations"),
-      direction: "up",
+    console.log("Entrou no POST");
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
       dryRun: false,
-      verbose: true,
     });
-    return response.status(200).json(migrations);
-  }
 
-  
-  return response.status(404).end();
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
 }
