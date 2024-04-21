87d20377439a92c1ca5464d7d262a1ca019fe0db
Sam Damazio <128763505+samdamazio@users.noreply.github.com>
2024-04-21 19:55:20 +0000

    aula dia 25

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,10 +1,12 @@
-import database from "infra/database";
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
   const defaultMigrationsOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     migrationsTable: "pgmigrations",
     dir: join("infra", "migrations"),
     direction: "up",
@@ -15,6 +17,7 @@ export default async function migrations(request, response) {
   if (request.method === "GET") {
     console.log("Entrou no GET");
     const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
 
@@ -24,6 +27,7 @@ export default async function migrations(request, response) {
       ...defaultMigrationsOptions,
       dryRun: false,
     });
+    await dbClient.end();
 
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
